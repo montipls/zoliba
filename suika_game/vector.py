6a436738ec53bc394ba73d35a08d6e19137bfd19
Monti <145486508+montipls@users.noreply.github.com>
2026-03-09 13:22:10 +0100

    fixed vector.py

--- suika_game/vector.py
+++ suika_game/vector.py
@@ -1,30 +1,30 @@
 class Vector:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
     def __add__(self, other):
-        if type(other) == float or type(other) == int:
-            return Vector(self.x + other, self.y + other)
         return Vector(self.x + other.x, self.y + other.y)
-    
+
     def __sub__(self, other):
-        if type(other) == float or type(other) == int:
-            return Vector(self.x - other, self.y - other)
         return Vector(self.x - other.x, self.y - other.y)
-    
-    def __mul__(self, other = float):
+
+    def __mul__(self, other: float):
         return Vector(self.x * other, self.y * other)
-    
-    def __truediv__(self, other = float):
+
+    def __truediv__(self, other: float):
         return Vector(self.x / other, self.y / other)
     
-    def __neg__(self):
-        return Vector(-self.x, -self.y)
+    def __getitem__(self, index: int) -> float:
+        return self.true()[index]
 
-    def tup(self):
+    def true(self) -> tuple:
         return (self.x, self.y)
     
-    @staticmethod
-    def new(t: tuple):
-        return Vector(t[0], t[1])
+    def change(self, axis, new) -> None:
+        if axis == 'x':
+            self.x = new
+        elif axis == 'y':
+            self.y = new
+        else:
+            raise IndexError
